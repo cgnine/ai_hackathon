f6b9c0e783c9d6aaac6e5030b7d3fba2a28aec73
cgnine <cgnine@gmail.com>
2026-05-28 21:12:46 +0900

    port 수정

--- frontend/app.js
+++ frontend/app.js
@@ -3,7 +3,7 @@
   if (window.location.protocol === "file:" || localHosts.has(window.location.hostname)) {
     return "http://localhost:8000";
   }
-  return "";
+  return `http://${window.location.hostname}:8000`;
 })();
 
 const DEMO_ATTEMPT_ID = "123456789";
